f032d968af523f3bb7604a2704dedc97edde5cfc
laithqud <laithqudahh@gmail.com>
2025-02-11 09:04:58 +0300

    js

--- javascript/script.js
+++ javascript/script.js
@@ -1,32 +1,4 @@
 
-// function validate() {
-//     let room = document.getElementById('room').value;
-//     let guests = document.getElementById('guests').value;
-//     let from = document.getElementById('from').value;
-//     let to = document.getElementById('to').value;
-    
-//     if (room.trim() === "") {
-//         alert('Please choose one of the rooms');
-//         return false;
-//     }
-//     if (guests <= 0) {
-//         alert('Please choose the right number of people');
-//         return false;
-//     }
-//     const today = new Date().setHours(0, 0, 0, 0);
-//     const fromDate = new Date(from).setHours(0, 0, 0, 0);
-//     const toDate = new Date(to).setHours(0, 0, 0, 0);
-//     if (fromDate < today) {
-//         alert('Please choose Arrivel date');
-//         return false;
-//     }
-//     if (toDate <= fromDate) {
-//         alert('Please choose Leaving date');
-//         return false;
-//     }
-    
-//     return true;
-// }
 
 function validate() {
     let room = document.getElementById('room').value;
